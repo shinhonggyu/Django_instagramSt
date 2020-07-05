2f82e80ddb1fa1dd075dc269ce3dfade2812932e
shinhonggyu <66147646+shinhonggyu@users.noreply.github.com>
2020-07-05 21:09:54 +0900

    커스텀 유저 저장 및 회원가입 구현

---
+++ accounts/views.py
@@ -0,0 +1,17 @@
+from django.contrib import messages
+from django.shortcuts import redirect, render
+from .forms import SignupForm
+
+
+def signup(request):
+    if request.method == 'POST':
+        form = SignupForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, "회원가입 환영합니다")
+            return redirect("root")
+    else:
+        form = SignupForm()
+    return render(request, 'accounts/signup_form.html', {
+        'form': form,
+    })
