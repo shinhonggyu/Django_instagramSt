8e2b2ec7f3c80b7284941853397f5a8808dc8e07
shinhonggyu <66147646+shinhonggyu@users.noreply.github.com>
2020-07-06 17:30:01 +0900

    프로필 수정 구현 , TextChoices

--- accounts/models.py
+++ accounts/models.py
@@ -1,7 +1,18 @@
 from django.contrib.auth.models import AbstractUser
+from django.core.validators import RegexValidator
 from django.db import models
 
 
 class User(AbstractUser):
+    class GenderChoices(models.TextChoices):
+        MALE = "M", "남성"
+        FEMALE = "F", "여성"
+
     website_url = models.URLField(blank=True)
     bio = models.TextField(blank=True)
+    phone_number = models.CharField(max_length=13, blank=True,
+                                    validators=[RegexValidator(r"^010-?[1-9]\d{3}-?\d{4}$")])
+    gender = models.CharField(max_length=1, blank=True,
+                              choices=GenderChoices.choices)
+    avatar = models.ImageField(
+        blank=True, upload_to="accounts/avatar/%Y/%m/%d", help_text="48px * 48px 크기의 png/jpg 파일을 업로드해주세요. ")
