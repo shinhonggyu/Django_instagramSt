36ad3c7ee43c776c09956ed04d6dfba0c389505e
shinhonggyu <66147646+shinhonggyu@users.noreply.github.com>
2020-07-06 14:04:04 +0900

    로그인, 로그아웃, 회원가입 동시 로그인

--- accounts/urls.py
+++ accounts/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    # /accounts/login => settings.LOGIN_URL
+    path('login/', views.login, name='login'),
+    path('logout/', views.logout, name='logout'),
     path('signup/', views.signup, name='signup'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,15 +1,27 @@
 from django.contrib import messages
+from django.contrib.auth import login as auth_login
+from django.contrib.auth.views import LoginView, logout_then_login
 from django.shortcuts import redirect, render
 from .forms import SignupForm
+
+
+login = LoginView.as_view(template_name="accounts/login_form.html")
+
+
+def logout(request):
+    messages.success(request, '로그아웃되었습니다.')
+    return logout_then_login(request)
 
 
 def signup(request):
     if request.method == 'POST':
         form = SignupForm(request.POST)
         if form.is_valid():
-            form.save()
+            signed_user = form.save()
+            auth_login(request, signed_user)
             messages.success(request, "회원가입 환영합니다")
-            return redirect("root")
+            next_url = request.GET.get('next', '/')
+            return redirect(next_url)
     else:
         form = SignupForm()
     return render(request, 'accounts/signup_form.html', {
