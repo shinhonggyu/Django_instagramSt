e44a74f6793019444c64b26decda71728e4579bb
shinhonggyu <66147646+shinhonggyu@users.noreply.github.com>
2020-07-06 21:26:01 +0900

    auth앱의PasswordChangeForm 커스텀을 통한 암호 수정 구현

--- accounts/urls.py
+++ accounts/urls.py
@@ -5,6 +5,7 @@ urlpatterns = [
     # /accounts/login => settings.LOGIN_URL
     path('login/', views.login, name='login'),
     path('logout/', views.logout, name='logout'),
+    path('password_change/', views.password_change, name='password_change'),
     path('signup/', views.signup, name='signup'),
     path('edit/', views.profile_edit, name='profile_edit'),
 ]
