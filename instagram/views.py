0dd68426e4520b6bbbf8fd65aa956111f76658ae
shinhonggyu <66147646+shinhonggyu@users.noreply.github.com>
2020-07-08 21:29:30 +0900

    easy-thumbnails로 유저페이지 썸네일 처리 구현

--- instagram/views.py
+++ instagram/views.py
@@ -36,7 +36,9 @@ def user_page(request, username):
     page_user = get_object_or_404(
         get_user_model(), username=username, is_active=True)
     post_list = Post.objects.filter(author=page_user)
+    post_list_count = post_list.count()  # 실제 DB에 count 쿼리를 던짐
     return render(request, "instagram/user_page.html", {
         "page_user": page_user,
         "post_list": post_list,
+        "post_list_count": post_list_count,
     })
