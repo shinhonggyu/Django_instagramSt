369ea5655bfbd46010a4b9af192fbefbf26f8c46
shinhonggyu <66147646+shinhonggyu@users.noreply.github.com>
2020-07-09 22:17:28 +0900

    유저페이지에서 Unfollow구현

--- instagram/views.py
+++ instagram/views.py
@@ -55,8 +55,15 @@ def user_page(request, username):
         get_user_model(), username=username, is_active=True)
     post_list = Post.objects.filter(author=page_user)
     post_list_count = post_list.count()  # 실제 DB에 count 쿼리를 던짐
+
+    if request.user.is_authenticated:
+        is_follow = request.user.following_set.filter(pk=page_user.pk).exists()
+    else:
+        is_follow = False
+
     return render(request, "instagram/user_page.html", {
         "page_user": page_user,
         "post_list": post_list,
         "post_list_count": post_list_count,
+        "is_follow": is_follow
     })
