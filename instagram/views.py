48e1692147194046be52137ce5e0a2899494dee3
shinhonggyu <66147646+shinhonggyu@users.noreply.github.com>
2020-07-09 22:58:48 +0900

    Timeline 포스팅 목록 조건에 날짜 범위 지정하기

--- instagram/views.py
+++ instagram/views.py
@@ -1,18 +1,24 @@
+from datetime import timedelta
 from django.contrib import messages
 from django.contrib.auth import get_user_model
 from django.contrib.auth.decorators import login_required
 from django.db.models import Q
 from django.shortcuts import get_object_or_404, redirect, render
+from django.utils import timezone
 from .forms import PostForm
 from .models import Post
 
 
 @login_required
 def index(request):
+    timesince = timezone.now() - timedelta(days=3)
     post_list = Post.objects.all()\
         .filter(
             Q(author=request.user) |
             Q(author__in=request.user.following_set.all())
+    )\
+        .filter(
+            created_at__gte=timesince  # greater than equal
     )
 
     suggested_user_list = get_user_model().objects.all()\
